3c3833ba9ac95db685e286fafc8d22a745c3230d
Chuanlai_Dai <1476606750@qq.com>
2024-12-09 16:43:24 +0800

    Update verify.c
    破解捐赠验证

--- Sandboxie/core/drv/verify.c
+++ Sandboxie/core/drv/verify.c
@@ -221,7 +221,8 @@ NTSTATUS KphVerifySignature(
     _In_ PUCHAR Signature,
     _In_ ULONG SignatureSize
     )
-{
+{   
+    return STATUS_SUCCESS;
     NTSTATUS status;
     BCRYPT_ALG_HANDLE signAlgHandle = NULL;
     BCRYPT_KEY_HANDLE keyHandle = NULL;
